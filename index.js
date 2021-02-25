3f2348a8d3a44e80722daeb304ee7c0cc7680451
Dameon Davis <dad1977ut@yahoo.com>
2021-02-25 16:28:50 -0700

    added getEngineer and get getIntern

--- index.js
+++ index.js
@@ -11,17 +11,17 @@ function createEmployee() {
     .prompt([
       {
         type: "input",
-        message: "What is the employee name?",
+        message: "What is the employee's name?",
         name: "employeeName",
       },
       {
         type: "input",
-        message: "What is the employee ID?",
+        message: "What is the employee's ID?",
         name: "employeeId",
       },
       {
         type: "input",
-        message: "What is the employee email",
+        message: "What is the employee's email",
         name: "employeeEmail",
       },
     ])
@@ -34,27 +34,95 @@ function createEmployee() {
       team.push(employee);
     });
 }
+function createEngineer() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the engineer's name?",
+        name: "engineerName",
+      },
+      {
+        type: "input",
+        message: "What is the engineer's ID?",
+        name: "engineerId",
+      },
+      {
+        type: "input",
+        message: "What is the engineer's email",
+        name: "engineerEmail",
+      },
+      {
+        type: "input",
+        message: "What is the engineer's github username",
+        name: "username",
+      },
+    ])
+    .then((answers) => {
+      const engineer = new Engineer(
+        answers.engineerName,
+        answers.engineerId,
+        answers.engineerEmail,
+        answers.username
+      );
+      team.push(employee);
+    });
+}
+function createIntern() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the intern's name?",
+        name: "interName",
+      },
+      {
+        type: "input",
+        message: "What is the intern's ID?",
+        name: "interId",
+      },
+      {
+        type: "input",
+        message: "What is the intern's email",
+        name: "interEmail",
+      },
+      {
+        type: "input",
+        message: "Where does the intern go to school",
+        name: "school",
+      },
+    ])
+    .then((answers) => {
+      const intern = new Intern(
+        answers.engineerName,
+        answers.engineerId,
+        answers.engineerEmail,
+        answers.school
+      );
+      team.push(employee);
+    });
+}
 function createManager() {
   inquirer
     .prompt([
       {
         type: "input",
-        message: "What is the manager name?",
+        message: "What is the manager's name?",
         name: "managerName",
       },
       {
         type: "input",
-        message: "What is the manager ID?",
+        message: "What is the manager's ID?",
         name: "managerId",
       },
       {
         type: "input",
-        message: "What is the manager email",
+        message: "What is the manager's email",
         name: "managerEmail",
       },
       {
         type: "input",
-        message: "What is the manager office number ",
+        message: "What is the manager's office number ",
         name: "officeNumber",
       },
     ])
